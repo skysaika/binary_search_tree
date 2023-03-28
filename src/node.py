b1cbc0473143e9ef0611d9006dedb8809755dbc9
Saysuu <skysaika@yandex.ru>
2023-03-28 20:51:59 +0300

    added typing to Node class

--- src/node.py
+++ src/node.py
@@ -4,5 +4,5 @@ class Node:
     def __init__(self, data: dict) -> None:
         """Сохраняю данные. Инициализирую пустые левый и правый атрибуты"""
         self.data = data
-        self.left = None
+        self.left: Node | None = None
         self.right = None
