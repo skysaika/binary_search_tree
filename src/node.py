15a5f2c185deb96e262aadf6c9d4a676825f2fc3
Saysuu <skysaika@yandex.ru>
2023-03-28 18:01:31 +0300

    added class Node and tests

---
+++ src/node.py
@@ -0,0 +1,8 @@
+class Node:
+    """Класс для хранения данных"""
+
+    def __init__(self, data: dict) -> None:
+        """Сохраняю данные. Инициализирую пустые левый и правый атрибуты"""
+        self.data = data
+        self.left = None
+        self.right = None
---
+++ tests/test_node.py
@@ -0,0 +1,9 @@
+from src.node import Node
+
+def test_node_init():
+    """Проверяем, что объект Node ининциализируется правильно"""
+    data = {'id': 40}
+    node = Node(data)
+    assert node.data == data
+    assert node.left is None
+    assert node.right is None
